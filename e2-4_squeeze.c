4350ca6f68b983aa3db00eb3bea1052ab20daeda
V_eunae <eajang90@gmail.com>
2015-07-23 08:34:20 +0000

    e2-4_squeeze.c

--- e2-4_squeeze.c
+++ e2-4_squeeze.c
@@ -14,16 +14,18 @@ void squeeze(char s1[], char s2[])
         j = 0;
         while(s2[j] != '\0')
         {
-            if (s1[i] == s2[j]) s1[i] = '\0';
+            if (s1[i] == s2[j]) s1[i] = ' ';
             else j++;
         }
+        i++;
     }
-    printf("%s", s1);
+    printf("%s\n", s1);
 }
 
 int main()
 {
     char s1[11] = "hello,there";
     char s2[3] = "ael";
+    squeeze(s1, s2);
     return 0;
 }
