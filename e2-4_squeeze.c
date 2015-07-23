e2bd2b45a146edbf17d9e9ff080dec4fc898ff20
eajang90 <eajang90@gmail.com>
2015-07-23 15:58:50 +0900

    [150723]

--- e2-4_squeeze.c
+++ e2-4_squeeze.c
@@ -1,9 +1,29 @@
 //
 //  e2-4_squeeze.c
-//  
-//
-//  Created by KETI-MAC on 2015. 7. 23..
 //
 //
 
 #include <stdio.h>
+
+void squeeze(char s1[], s2[])
+{
+    int i, j;
+    i = 0;
+    while (s1[i] != '\0')
+    {
+        j = 0;
+        while(s2[j] != '\0')
+        {
+            if (s1[i] == s2[j]) s1[i] = '\0';
+            else j++;
+        }
+    }
+    printf("%s", s1);
+}
+
+int main()
+{
+    char s1[11] = "hello,there";
+    char s2[3] = "ael";
+    return 0;
+}
